7454de1830542025c20abfaea38bf877c9c7390c
Juan Manuel Serruya <shuank@gmail.com>
2014-07-09 15:17:32 +0200

    initial commit

---
+++ NativeInstrumentsTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  NativeInstrumentsTest
+//
+//  Created by Juan Manuel Serruya on 7/9/14.
+//  Copyright (c) 2014 NativeInstrumentsTest. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
